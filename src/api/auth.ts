f70a20cf7aa2564424780c41c922b2d3c5e1ca97
inyoungfriend <happy5225@naver.com>
2024-04-03 15:46:07 +0900

    ⚡️ 성능 개선

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -7,13 +7,12 @@ import axios, {
 } from 'axios';
 ;
 
-const baseUrl = import.meta.env.VITE_SERVER_URL;
+const baseUrl = process.env.VITE_SERVER_URL;
 
 export const axiosInstance: AxiosInstance = axios.create({
   baseURL: baseUrl,
   withCredentials: true,
 });
-
 
 
 
