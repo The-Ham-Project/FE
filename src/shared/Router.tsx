67c0bad632b9b74794b8bfbcc43e8192aecd89f2
inyoungfriend <happy5225@naver.com>
2024-04-04 16:49:05 +0900

    FEAT: 내위치 기능 추가

--- src/shared/Router.tsx
+++ src/shared/Router.tsx
@@ -4,6 +4,7 @@ import KakaoLogin from '../pages/Login/KakaoLogin';
 import NaverLogin from '../pages/Login/NaverLogin';
 import GoogleLogin from '../pages/Login/GoogleLogin';
 import Location from '../pages/Location/Location';
+// import RoutingMap from '../pages/Location/RoutingMap.tsx';
 import Redirect from '../components/KakaoLogin/Redirect';
 import PostDetailsPage from '../pages/main/PostDetailsPage';
 import Details from '../pages/main/Details';
@@ -19,6 +20,7 @@ function Router() {
         <Route path="/oauth/kakaologin" element={<KakaoLogin />} />
         <Route path="/oauth/naverlogin" element={<NaverLogin />} />
         <Route path="/oauth/googlelogin" element={<GoogleLogin />} />
+        {/* <Route path="/hellokakaoap" element={<RoutingMap />} /> */}
         <Route path="/thxkakaomap" element={<Location />} />
         <Route path="/details/:rentalId" element={<Details />} />
         <Route path="/kakao/callback" element={<Redirect />} />
