4fe560ef824af417585a637a4607bb3f58144d41
zook0320 <zook020320@gmail.com>
2024-04-18 19:47:07 +0900

    중간저장

--- src/components/Main/Search.tsx
+++ src/components/Main/Search.tsx
@@ -6,9 +6,9 @@ import lgoo from '/public/assets/lgoo.svg';
 import { IoPersonOutline } from 'react-icons/io5';
 import styled from 'styled-components';
 
-const Search = () => {
-  const navigate = useNavigate();
+function Search() {
   const [keyword, setKeyword] = useState('');
+  const navigate = useNavigate();
   const [showInput, setShowInput] = useState(false); // 인풋 창 보이기 여부를 관리하는 상태
 
   const { data, isLoading, isError } = useQuery({
@@ -32,81 +32,93 @@ const Search = () => {
     enabled: keyword !== '', // keyword가 변경되면 쿼리를 다시 실행합니다.
   });
 
+  // const toggleInput = () => {
+  //   setShowInput(!showInput); // showInput 상태를 토글하여 인풋 창 보이기 여부 변경
+  // };
+
+
   const handleChange = (e) => {
     setKeyword(e.target.value);
+    console.log(e.target.value);
+    //서치디테일페이지에전해줌
   };
 
-  const handleSearch = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    if (keyword.trim() === '') return alert('검색어를 입력하세요');
+  const handleSearch = () => {
+    setShowInput(true);
+    if (!showInput) return;
+    if (keyword.trim() === '') return alert('검색어를 적어주세요');
+    navigate(`/search?keyword=${encodeURIComponent(keyword)}&page=1&size=6`);
   };
 
-  const toggleInput = () => {
-    setShowInput(!showInput); // showInput 상태를 토글하여 인풋 창 보이기 여부 변경
+ 
+
+  const activeEnter = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === 'Enter') {
+      handleSearch();
+    }
   };
 
   return (
-    <div style={{display: 'flex', backgroundColor: 'white'}}> 
-    <img
-    style={{
-      width: '59px',
-      height: '59px',
-
-    }}
-    src={lgoo}
-  />
-    <SearchContainer>
-     
-      
-      {/* showInput 상태에 따라 인풋 창을 표시하거나 숨깁니다. */}
-      <AnimatedInputContainer showInput={showInput}>
-        <Input
-          type="text"
-          placeholder="검색..."
-          value={keyword}
-          onChange={handleChange}
-        />
-      </AnimatedInputContainer>
-
-      <SearchButton
-        onClick={toggleInput}
+    <div style={{ display: 'flex', backgroundColor: 'white' }}>
+      <img
         style={{
-          backgroundColor: '#F5F5F5',
-          maxWidth: '59px',
-          maxHeight: '59px',
+          width: '59px',
+          height: '59px',
         }}
-      >
-        <img
-          style={{
-            maxWidth: '22px',
-            maxHeight: '22px',
-          }}
-          src={searchIcon}
-        />
-      </SearchButton>
-
-      {/* 검색 결과 표시 */}
-      <SearchResults>
-        {isLoading && <li>Loading...</li>}
-        {isError && <li>Error occurred while fetching data</li>}
-        {data &&
-          data.searchResponseList.map((rental) => (
-            <li key={rental.rentalId}>
-              <Link to={`/Details/${rental.rentalId}`}>{rental.title}</Link>
-            </li>
-          ))}
-      </SearchResults>
- 
-      <IoPersonOutline
-        size={'22px'}
-        onClick={() => {
-          navigate('/mypage');
-        }}
+        src={lgoo}
       />
-    </SearchContainer>
+      <SearchContainer>
+        <AnimatedInputContainer showInput={showInput}>
+          <input
+            onKeyDown={activeEnter}
+            type="text"
+            placeholder="검색..."
+            value={keyword}
+            onChange={handleChange}
+          />
+        </AnimatedInputContainer>
+        <SearchButton
+          onClick={() => {
+           
+            handleSearch();
+          }}
+          style={{
+            backgroundColor: '#F5F5F5',
+            maxWidth: '59px',
+            maxHeight: '59px',
+          }}
+        >
+          <img
+            style={{
+              maxWidth: '22px',
+              maxHeight: '22px',
+            }}
+            src={searchIcon}
+          />
+        </SearchButton>
+
+        {/* 검색 결과 표시 */}
+        <SearchResults>
+          {isLoading && <li>Loading...</li>}
+          {isError && <li>Error occurred while fetching data</li>}
+          {data &&
+            data.searchResponseList?.map((rental) => (
+              <li key={rental.rentalId}>
+                <Link to={`/Details/${rental.rentalId}`}>{rental.title}</Link>
+              </li>
+            ))}
+        </SearchResults>
+
+        <IoPersonOutline
+          size={'22px'}
+          onClick={() => {
+            navigate('/mypage');
+          }}
+        />
+      </SearchContainer>
     </div>
   );
-};
+}
 
 export default Search;
 
@@ -117,23 +129,19 @@ const SearchContainer = styled.div`
   align-items: center;
   background-color: white;
   justify-content: flex-end;
-  
 `;
 
 const AnimatedInputContainer = styled.div<{ showInput: boolean }>`
-  width: ${({ showInput }) => (showInput ? '100%' : '0')}; /* 입력 창의 너비를 조정합니다. */
+  width: ${({ showInput }) =>
+    showInput ? '100%' : '0'}; /* 입력 창의 너비를 조정합니다. */
   opacity: ${({ showInput }) => (showInput ? 1 : 0)};
   overflow: hidden; /* 애니메이션 중 내용물이 넘치는 것을 방지합니다. */
   transition: width 0.3s ease-in-out; /* 너비 변경에 대한 애니메이션 효과를 적용합니다. */
 `;
 
-const Input = styled.input`
-  
-`;
+const Input = styled.input``;
 
-const SearchButton = styled.button`
-  
-`;
+const SearchButton = styled.button``;
 
 const SearchResults = styled.ul`
   /* 검색 결과 스타일 */
