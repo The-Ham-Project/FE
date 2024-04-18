d380c23a655e17f4e07ab011f2a18bd4b5186025
zook0320 <zook020320@gmail.com>
2024-04-18 21:27:52 +0900

    버그수정2

--- src/api/search.ts
+++ src/api/search.ts
@@ -2,26 +2,6 @@ import { instance } from './axios';
 import { useQuery } from '@tanstack/react-query';
 
 export const getKeywordList = async (keyword) => {
-<<<<<<< HEAD
-return await instance
-.get(`/api/v1/rentals/search?keyword=${keyword}&page=1&size=6`)
-.then((response) => response.data)
-.catch((error) => {
-throw error;
-});
-};
-// const searchPosts = async (query: string) => {
-// const { data } = await instance.get(`/posts/search?query=${query}`);
-// return data;
-// };
-
-// export const useSearchedPosts = (query: string) => {
-// return useQuery({
-// queryKey: ['searchedPosts', query],
-// queryFn: () => searchPosts(query),
-// });
-// };
-=======
   return await instance
     .get(`/api/v1/rentals/search?keyword=${keyword}&page=1&size=6`)
     .then((response) => response.data)
@@ -40,4 +20,3 @@ throw error;
 //     queryFn: () => searchPosts(query),
 //   });
 // };
->>>>>>> dev
