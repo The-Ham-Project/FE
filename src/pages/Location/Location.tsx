96ab043c4f36dfb0896a87c1d4587b78dd48b313
inyoungfriend <happy5225@naver.com>
2024-04-20 01:59:49 +0900

    ✨ 새 기능

--- src/pages/Location/Location.tsx
+++ src/pages/Location/Location.tsx
@@ -93,10 +93,27 @@ export default function Location(): JSX.Element {
       fillColor: 'rgb(0,26,255)',
       fillOpacity: 0.05,
     });
+
     circle.setMap(null);
     marker.setMap(null);
     circle.setMap(map);
     marker.setMap(map);
+    // 지도에 클릭 이벤트를 등록합니다
+    // 지도를 클릭하면 마지막 파라미터로 넘어온 함수를 호출합니다
+    window.kakao.maps.event.addListener(map, 'click', function (mouseEvent) {
+      // 클릭한 위도, 경도 정보를 가져옵니다
+      const latlng = mouseEvent.latLng;
+
+      // 마커 위치를 클릭한 위치로 옮깁니다
+      marker.setPosition(latlng);
+      circle.setPosition(latlng);
+
+      let message = '클릭한 위치의 위도는 ' + latlng.getLat() + ' 이고, ';
+      message += '경도는 ' + latlng.getLng() + ' 입니다';
+      console.log(message);
+      // const resultDiv = document.getElementById('clickLatlng');
+      // resultDiv.innerHTML = message;
+    });
 
     navigator.geolocation.getCurrentPosition((pos) => {
       alterAddress(pos);
