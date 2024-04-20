5863811d2ad622c5fa831dff46d8869234e22b1b
inyoungfriend <happy5225@naver.com>
2024-04-21 02:05:30 +0900

    ㅠㅠ

--- src/pages/Mypage/MyList.tsx
+++ src/pages/Mypage/MyList.tsx
@@ -11,7 +11,7 @@ import Modal from '../../components/modal/Modal.tsx';
 import modification from '../../../public/assets/modification.svg';
 import trashbin from '../../../public/assets/trashbin.svg';
 import DeleteModal from '../../components/modal/DeleteModal.tsx';
-import { deleteRental } from '../../api/mylist.ts';
+import { removeItemPost } from '../../api/itemAPI.ts';
 
 interface Rental {
   rentalId: number;
@@ -82,7 +82,7 @@ function MyList() {
     closeModal();
     const deleteMutation = useMutation({
       mutationFn: async (rentalId: number) => {
-        await deleteRental(rentalId);
+        await removeItemPost(rentalId);
       },
       onSuccess: () => {
         // 삭제 성공 후 로직
